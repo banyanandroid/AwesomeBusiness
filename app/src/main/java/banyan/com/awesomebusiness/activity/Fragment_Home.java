c8360dba022936a2885320009fbbdd3e850edbf4
chandru <chandru@banyaninfotech.com>
2017-07-21 14:20:08 +0530

    21-st July (sort by -update)

--- app/src/main/java/banyan/com/awesomebusiness/activity/Fragment_Home.java
+++ app/src/main/java/banyan/com/awesomebusiness/activity/Fragment_Home.java
@@ -5,10 +5,16 @@ package banyan.com.awesomebusiness.activity;
  */
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.aromajoin.actionsheet.ActionSheet;
+import com.aromajoin.actionsheet.OnActionListener;
 
 import banyan.com.awesomebusiness.R;
 
@@ -23,6 +29,7 @@ public class Fragment_Home extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
@@ -30,9 +37,49 @@ public class Fragment_Home extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        Button button = (Button) rootView.findViewById(R.id.btn_sort);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                showActionSheet(view);
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    private void showActionSheet(View anchor) {
+        ActionSheet actionSheet = new ActionSheet(getActivity());
+        actionSheet.setTitle("sort by");
+        actionSheet.setSourceView(anchor);
+        actionSheet.addAction("Newest", ActionSheet.Style.DEFAULT, new OnActionListener() {
+            @Override public void onSelected(ActionSheet actionSheet, String title) {
+                performAction(title);
+                actionSheet.dismiss();
+            }
+        });
+        actionSheet.addAction("Oldest", ActionSheet.Style.DEFAULT, new OnActionListener() {
+            @Override public void onSelected(ActionSheet actionSheet, String title) {
+                performAction(title);
+                actionSheet.dismiss();
+            }
+        });
+
+        actionSheet.addAction("Relevant", ActionSheet.Style.DESTRUCTIVE, new OnActionListener() {
+            @Override public void onSelected(ActionSheet actionSheet, String title) {
+                performAction(title);
+                actionSheet.dismiss();
+            }
+        });
+
+        actionSheet.show();
+    }
+
+    private void performAction(String title) {
+      /*  Snackbar.make(MainActivity.this.findViewById(android.R.id.content), title,
+               Snackbar.LENGTH_SHORT).show();*/
+        Toast.makeText(getActivity(), "Sort By "+ title + " Content ", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
