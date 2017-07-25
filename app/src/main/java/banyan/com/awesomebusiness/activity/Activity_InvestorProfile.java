e5bfe448e63c8f747f6e70ff8d7135067efaa759
chandru <chandru@banyaninfotech.com>
2017-07-25 19:05:19 +0530

    25/july - Forms Update (Business,Franchise,Investor - Profiles)

--- app/src/main/java/banyan/com/awesomebusiness/activity/Activity_InvestorProfile.java
+++ app/src/main/java/banyan/com/awesomebusiness/activity/Activity_InvestorProfile.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import banyan.com.awesomebusiness.R;
@@ -16,12 +17,14 @@ public class Activity_InvestorProfile extends AppCompatActivity {
 
 
     Button btn_submit;
+    EditText edt_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_investor_profile);
 
+        edt_name = (EditText) findViewById(R.id.edt_name);
         btn_submit = (Button)findViewById(R.id.btn_submit);
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
